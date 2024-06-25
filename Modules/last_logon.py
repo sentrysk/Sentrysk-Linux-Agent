67ccac5d99bf58a2d892ef2eeb353ce109b1969a
keraattin <kerem-oruc@outlook.com>
2024-06-25 18:28:39 +0300

    #15 last logon date collect method changed to regex

--- Modules/last_logon.py
+++ Modules/last_logon.py
@@ -3,24 +3,41 @@
 # Libraries
 ##############################################################################
 import subprocess
-import json
+import re
+from datetime import datetime
 ##############################################################################
 
+# Global Values
+##############################################################################
+TIME_REGEX = r'\b(?:Mon|Tue|Wed|Thu|Fri|Sat|Sun) (?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d{1,2} \d{2}:\d{2}:\d{2} \d{4}\b'
+# Compile the pattern
+TM_RE = re.compile(TIME_REGEX)
+##############################################################################
 
 # Functions
 ##############################################################################
 def get_last_logons():
     last_logons = []
 
-    for line in subprocess.check_output(["last", "-w"]).decode("utf-8").splitlines():
+    for line in subprocess.check_output(["last", "-F"]).decode("utf-8").splitlines():
         fields = line.split()
         if len(fields) >= 7:
             user = fields[0]
-            last_logon = f"{fields[3]} {fields[4]} {fields[5]}"
-            last_logons.append({
-                "username": user, 
-                "last_logon": last_logon
-            })
+            # Skip reboot
+            if user.lower() == "reboot" or user.lower() == "shutdown":
+                continue
+            
+            # Check if Time Regex matches
+            match = re.search(TM_RE, line)
+            if match:
+                last_logon = match.group(0)
+                last_logon = datetime.strptime(last_logon, '%a %b %d %H:%M:%S %Y')
+
+                
+                last_logons.append({
+                    "username": user,
+                    "last_logon": last_logon.strftime("%Y-%m-%d %H:%M:%S")
+                })
 
     return last_logons
 ##############################################################################
